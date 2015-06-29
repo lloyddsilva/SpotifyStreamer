682c681cfc0e08083c0b4db242fd770f868b8eae
Lloyd D'Silva <lloyddsilva@gmail.com>
2015-06-29 03:04:48 -0700

    Linked up activites and added images

--- app/src/main/java/com/lloyddsilva/spotifystreamer/ArtistResultsActivity.java
+++ app/src/main/java/com/lloyddsilva/spotifystreamer/ArtistResultsActivity.java
@@ -1,22 +1,27 @@
 package com.lloyddsilva.spotifystreamer;
 
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ResultsActivity extends ActionBarActivity {
+public class ArtistResultsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_results);
+        setContentView(R.layout.activity_artist_results);
+
+        handleIntent(getIntent());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_results, menu);
+        getMenuInflater().inflate(R.menu.menu_artist_results, menu);
         return true;
     }
 
@@ -33,5 +38,22 @@ public class ResultsActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        // Verify the action and get the query
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+    }
+
+    public void doSearch(String query) {
+
     }
 }
--- app/src/main/java/com/lloyddsilva/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/lloyddsilva/spotifystreamer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.lloyddsilva.spotifystreamer;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
@@ -17,6 +21,15 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
